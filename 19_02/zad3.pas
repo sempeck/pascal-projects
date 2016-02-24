696b9f463c62a1d38a20622226700b23fb09cdb6
sempeck <jarek.sepek@mediorama.pl>
2016-02-24 17:53:09 +0100

    Ciąg Fibonacciego

--- 19_02/zad3.pas
+++ 19_02/zad3.pas
@@ -5,14 +5,21 @@
 // 1+1+2+3+5+8
 
 program zadanie3;
+var
+  n: Integer;
 
-function fib(): Integer;
+function fib(n: Integer): Integer;
 begin
-  
+  if (n<3) then
+    fib := 1
+  else
+    begin
+    fib := fib(n-2) + fib(n-1);
+    end;
 end;
 
 begin
   writeln('Podaj n: ');
   readln(n);
-  writeln(n, ' element ciągu Fibonacciego to ' fib(n));
+  writeln(n, ' element ciągu Fibonacciego to ', fib(n));
 end.
