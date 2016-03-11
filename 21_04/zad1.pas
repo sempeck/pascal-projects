09a3ee61f33e50b973afef182fe35c44ed8d84f8
sempeck <jarek.sepek@mediorama.pl>
2016-03-11 11:58:35 +0100

    Treść

---
+++ 21_04/zad1.pas
@@ -0,0 +1,21 @@
+// Lista 4
+// Zadanie 1. Zadeklarować typy danych określające listę jednokierunkową:
+// lista = ^tlista;
+// tlista = record
+// imie: string[20];
+// nazwisko: string[30];
+// wiek:byte;
+// PESEL: string;
+// wsk: lista;
+// end;
+// Zdefiniować zmienną pocz typu wskazującą na początek listy.
+// Wykorzystać przedstawione definicje do implementacji stosu. Opracować procedury:
+// a) dodającą element na stos
+// b) zdejmującą element ze stosu
+// c) wyprowadzającą zawartość stosu na ekran.
+// d) wyprowadzającą elementy stosu na ekran, ale tylko te, których nazwiska są
+// dłuższe od imion.
+// e) wyprowadzającą elementy stosu na ekran, wcześniej sortując wg numeru
+// PESEL
+// Przetestować opracowane procedury w programie głównym. Przed zakończeniem
+// programu zwolnić pamięć. Wyświetlić stan pamięci na początku i na końcu programu. 
